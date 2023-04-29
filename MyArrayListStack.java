cd534bc6fb56820166f247ba4c3d2d88a5651449
Elvira <adilovaelvira123@gmail.com>
2023-04-29 18:11:27 +0600

    importing the necessary classes

--- MyArrayListStack.java
+++ MyArrayListStack.java
@@ -1,2 +1,4 @@
+import java.util.EmptyStackException;
+import java.util.ArrayList;
 public class MyArrayListStack {
 }
--- MyLinkedListStack.java
+++ MyLinkedListStack.java
@@ -1,2 +1,4 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
 public class MyLinkedListStack {
 }
