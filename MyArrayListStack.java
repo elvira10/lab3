86c89841a000bf9c12416b10e9a35c8a2c718eb5
Elvira <adilovaelvira123@gmail.com>
2023-04-29 18:52:45 +0600

    creating a constructor for the class and initialize the instance variable

--- MyArrayListStack.java
+++ MyArrayListStack.java
@@ -2,4 +2,7 @@ import java.util.EmptyStackException;
 import java.util.ArrayList;
 public class MyArrayListStack<E> {
     private ArrayList<E> list;
+    public MyArrayListStack() {
+        list = new ArrayList<>();
+    }
 }
