ee4b71867ff8b9f0cd9d5b6d9f0e3337f4299ee4
Elvira <adilovaelvira123@gmail.com>
2023-04-30 22:23:07 +0600

    creating a constructor for the class and initialize the instance variable

--- MyLinkedListQueue.java
+++ MyLinkedListQueue.java
@@ -2,5 +2,7 @@ import java.util.NoSuchElementException;
 import java.util.ArrayList;
 public class MyLinkedListQueue<E> {
     private MyLinkedListQueue<E> queueList;
-
+    public MyLinkedListQueue() {
+        queueList = new MyLinkedListQueue<E>();
+    }
 }
