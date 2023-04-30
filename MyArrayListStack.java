4c8065cbde66e212d382b2bd35c8b219b8705ff4
Elvira <adilovaelvira123@gmail.com>
2023-04-30 23:25:03 +0600

    including documentation for stack

--- MyArrayListStack.java
+++ MyArrayListStack.java
@@ -3,23 +3,53 @@ import java.util.ArrayList;
 public class MyArrayListStack<E> {
     private ArrayList<E> list;
     public MyArrayListStack() {
-        list = new ArrayList<>();
+        list = new ArrayList<>(); //initializing the ArrayList with constructor
     }
+
+    /**
+     * pushes an element onto the top of the stack
+     * @param element the element to push onto the stack
+     */
     public void push(E element) {
+
         list.add(element);
     }
+
+    /**
+     * checks if the stack is empty
+     * @return true if the stack is empty, false otherwise
+     */
     public boolean isEmpty() {
+
         return list.isEmpty();
     }
+
+    /**
+     * returns the number of elements in the stack
+     * @return the size of the stack
+     */
     public int size() {
+
         return list.size();
     }
+
+    /**
+     * removes and returns the element at the top of the stack
+     * @return the element at the top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public E pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return list.remove(size() - 1);
     }
+
+    /**
+     * returns the element at the top of the stack without removing it
+     * @return the element at the top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public E peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
--- MyLinkedListStack.java
+++ MyLinkedListStack.java
@@ -4,26 +4,54 @@ public class MyLinkedListStack<E> {
     private LinkedList<E> list;
 
     public MyLinkedListStack() {
-        list = new LinkedList<>();
+
+        list = new LinkedList<>(); //initializing the linked list
     }
+
+    /**
+     * adds an element to the top of the stack
+     * @param element the element to add to the stack
+     */
     public void push(E element) {
         list.addFirst(element);
     }
+
+    /**
+     * checks if the stack is empty
+     * @return true if the stack is empty, false otherwise
+     */
     public boolean isEmpty() {
         return list.isEmpty();
     }
+
+    /**
+     * removes and returns the top element of the stack
+     * @return the top element of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public E pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return list.removeFirst();
     }
+
+    /**
+     * returns the top element of the stack without removing it
+     * @return the top element of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public E peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return list.getFirst();
     }
+
+    /**
+     * returns the size of the stack
+     * @return the size of the stack
+     */
     public int size() {
         return list.size();
     }
