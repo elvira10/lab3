2e484cd652cd95376d92e4c9262108eae5b35109
Elvira <adilovaelvira123@gmail.com>
2023-04-30 22:54:20 +0600

    creating a constructor for the class and initialize the instance variable

--- MyArrayListQueue.java
+++ MyArrayListQueue.java
@@ -3,5 +3,8 @@ import java.util.ArrayList;
 
 public class MyArrayListQueue<E> {
     private ArrayList<E> queueList;
+    public MyArrayListQueue() {
+        queueList = new ArrayList<E>();
+    }
 
 }
